4940e8a5b66d047cd732ca9222fba334e0425de5
haxor211 <bolo12345@gmail.com>
2017-05-16 15:09:49 +0200

    arena v2

--- js/arena_v2.js
+++ js/arena_v2.js
@@ -1,10 +1,27 @@
 // @ts-check
+// @ts-ignore
+// Welcome Msg
 
-var user = function (name, class, dmg, lvl, stamina, eq) {
+$(document).ready(function () {
+    
+});
+
+var log = function (log) { 
+    // @ts-ignore
+    $('#okienko').append('<p>' + log + '</p>');
+
+    };
+
+var user = function (name, race, dmg, lvl, stamina, eq) {
 
     this.name = name;
+    this.race = race;
+    this.dmg = dmg;
+    this.lvl = lvl;
+    this.stamina = stamina;
+    this.eq = eq;
+};
 
-}
+var player = new user("Bolo", "Human", 100, 10, 50, "Gladiator's Sword");
 
-var player = new user("Bolo");
-alert(player);
+log(player.name);
